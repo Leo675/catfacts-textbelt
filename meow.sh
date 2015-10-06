618b123f3109a6cba68ec952a02cff0110edffb6
root <root@localhost.localdomain>
2015-10-06 02:01:20 -0700

    multiple comma seperated numbers

--- meow.sh
+++ meow.sh
@@ -5,18 +5,21 @@ then
     echo 'TOR not running, Requires torsocks too'
     exit 0
 fi
-valid_number="^[0-9]{10}$"
+
 #Might not work outside US?
-if [[ ! $1 =~ $valid_number ]]
-then
-    echo Invalid phone number
-    exit 0
-else
+valid_number="^[0-9]{10}$"
     while read fact
     do
-        echo -e "sending fact: '$fact' to $1"
-        torsocks curl -X POST http://textbelt.com/text -d number=$1 -d "message=$fact"
-        pidof tor | xargs sudo kill -HUP
-        sleep $(( ( RANDOM % 500 )  + 100 ))
-    done <<< "$(shuf catfacts.txt)"
-fi
+    while read number
+    do
+        if [[ ! $number =~ $valid_number ]]
+        then
+            echo Invalid phone number $number
+            continue
+        fi
+        echo -e "sending fact: '$fact' to $number"
+        torsocks curl -X POST http://textbelt.com/text -d number=$number -d "message=$fact"
+    done <<< "$(echo $1 | sed 's/,/\n/g')"
+    pidof tor | xargs sudo kill -HUP
+    sleep $(( ( RANDOM % 500 )  + 100 ))
+done <<< "$(shuf catfacts.txt)"
